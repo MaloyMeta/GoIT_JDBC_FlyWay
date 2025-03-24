6be38660d4d64b7f8c8952a5241bcc3c48ca4bcd
lastwarrior228 <supermaks2211@gmail.com>
2025-03-24 12:51:58 +0200

    connection to db

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -6,28 +6,11 @@ public class Client {
 
     public Client(long id, String name){
         this.id = id;
-        this.setName(name);
+        this.name = (name);
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        validateName(name);
-        this.name = name;
-    }
-
-    public static void validateName(String name){
-        if(name == null || name.trim().isEmpty()){
-            throw new IllegalArgumentException("Ім'я не може бути пустим!");
-        }
-        if (name.length() < 2 || name.length() > 100){
-            throw new IllegalArgumentException("Ім'я закоротке(менше 2 символів) або задовге(більше 100 символів)");
-        }
+    @Override
+    public String toString(){
+        return "ID: " + id + " Name: " + name ;
     }
 }
--- src/main/java/org/example/ClientService.java
+++ src/main/java/org/example/ClientService.java
@@ -1,41 +1,80 @@
 package org.example;
 
+import java.sql.*;
 import java.util.*;
 
 public class ClientService {
-    private final Map<Long,Client> clients = new HashMap<>();
-    private long idCounter = 0;
+    private final Connection connection;
+    private final PreparedStatement createSt;
+    private final PreparedStatement readSt;
+    private final PreparedStatement updateSt;
+    private final PreparedStatement deleteSt;
+    private final PreparedStatement listAllSt;
 
-    long create(String name){
-        Client.validateName(name);
-        long id = idCounter++;
-        clients.put(id,new Client(id,name));
-        return id;
-    }
-    String getById(long id){
-        Client client = clients.get(id);
-        if(client == null){
-            throw new NoSuchElementException("Клієнта з ID: " + id + " немає");
-        }
-        return client.getName();
-    }
-    void setName(long id, String name){
-        Client.validateName(name);
-        Client client = clients.get(id);
-        if(client == null){
-            throw new NoSuchElementException("Клієнта з ID: " + id + " немає");
-        }
-        client.setName(name);
-    }
-    void deleteById(long id){
-        Client client = clients.get(id);
-        if (client == null){
-            throw new NoSuchElementException("Клієнта з ID: " + id + " немає");
-        }
-        clients.remove(id);
-    }
-    List<Client> listAll(){
-        return new ArrayList<>(clients.values());
+    public ClientService() throws SQLException{
+        this.connection = Database.getInstance().getConnection();
+        this.createSt = connection.prepareStatement("INSERT INTO CLIENT (NAME) VALUES (?)", Statement.RETURN_GENERATED_KEYS);
+        this.readSt = connection.prepareStatement("SELECT NAME FROM CLIENT WHERE ID = ?");
+        this.updateSt = connection.prepareStatement("UPDATE CLIENT SET NAME = ? WHERE ID = ?");
+        this.deleteSt = connection.prepareStatement("DELETE FROM CLIENT WHERE ID = ?");
+        this.listAllSt = connection.prepareStatement("SELECT ID, NAME FROM CLIENT");
     }
 
+    public long create(String name) throws SQLException{
+        validateName(name);
+        createSt.setString(1,name);
+        createSt.executeUpdate();
+
+        try(ResultSet rs = createSt.getGeneratedKeys()){
+            if (rs.next()){
+                return rs.getLong(1);
+            }
+        }
+        throw new SQLException("не вдалося створити клієнта");
+
+    }
+
+    public String getById(long id) throws SQLException{
+        readSt.setLong(1,id);
+        try(ResultSet rs = readSt.executeQuery()){
+            if (rs.next()){
+                return rs.getString("NAME");
+            }
+        }
+        throw new NoSuchElementException("Клієнта з ID: " + id + " немає");
+    }
+
+    public void setName(long id, String name) throws SQLException{
+        validateName(name);
+        updateSt.setLong(2,id);
+        updateSt.setString(1,name);
+        if (updateSt.executeUpdate() == 0){
+            throw new NoSuchElementException("Клієнта з ID: " + id + " немає");
+        }
+    }
+
+    public void deleteById(long id) throws SQLException{
+        deleteSt.setLong(1,id);
+        if (deleteSt.executeUpdate() == 0){
+            throw new NoSuchElementException("Клієнта з ID: " + id + " немає");
+        }
+    }
+
+    public List<Client> listAll() throws SQLException{
+        List<Client> clients = new ArrayList<>();
+        try (ResultSet rs = listAllSt.executeQuery()){
+            while(rs.next()){
+                clients.add(new Client(rs.getLong("ID"),rs.getString("NAME")));
+            }
+        }
+        return clients;
+    }
+    public void validateName(String name){
+        if (name == null || name.trim().isEmpty()){
+            throw new IllegalArgumentException("Пусте поле");
+        }
+        if (name.length() < 2 || name.length() > 100){
+            throw new IllegalArgumentException("Ім'я менше 2 або більше 100 символів");
+        }
+    }
 }
