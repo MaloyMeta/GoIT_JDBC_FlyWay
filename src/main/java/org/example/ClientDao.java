ac78fe51a6ad990d041596cf238464b5e130e668
MaloyMeta <supermaks2211@gmail.com>
2025-03-25 20:54:12 +0200

    update

--- src/main/java/org/example/ClientDao.java
+++ src/main/java/org/example/ClientDao.java
@@ -4,10 +4,16 @@ import java.sql.*;
 import java.util.*;
 
 public interface ClientDao {
+
     long create(String name) throws SQLException;
+
     String getById(long id) throws SQLException;
+
     void setName(long id, String name) throws SQLException;
+
     void deleteById(long id) throws SQLException;
+
     List<Client> listAll() throws SQLException;
+
 }
 
